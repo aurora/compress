cc464818e230e1097d3d7257926af6cd0837c6ca
Harald Lapp <h.lapp@clipdealer.de>
2019-03-15 13:50:06 +0100

    initial commit

---
+++ compress.sh
@@ -0,0 +1,144 @@
+#!/usr/bin/env bash
+
+NO_COMPRESS=(
+    "7z"  "ace" "ape"   "apk"  "arc" "arj" "avi" 
+    "bz2" "cap" "dar"   "deb"  "dmg" "exe" "flac"
+    "flv" "gz"  "ha"    "ice"  "iso" "jar" "jpg"  "jpeg"
+    "lha" "lz"  "lzh"   "lzma" "lzo" 
+    "m4a" "m4v" "mkv"   "mov"  "mp3" "mp4" "ogg"
+    "pak" "png" "rar"   "rpm"  "rz" 
+    "s7z" "sea" "sfark" "sit"  "sitx" "sqx" "sz"
+    "tbz" "tgz" "txz"
+    "wmv" "xz"  "zip"   "zoo" "zz"
+)
+
+NAME=""
+SOURCE=""
+TARGET=""
+
+function showusage {
+    echo "usage: $(basename $0) [arguments]
+
+-s, --source    The directory to compress.
+-t, --target    Optional target directory. Defaults to the parent directory
+                of the specified root directory, but cannot be the same.
+-n, --name      Optional name. Defaults to the name of the specified root
+                directory.
+-h, --help      Display this usage information.
+"
+}
+
+if [ "$1" = "" ]; then
+    showusage
+    exit 1
+fi
+
+while [[ "$1" != "" ]]; do
+    case $1 in
+        -s|--source)
+            if [ ! -d "$2" ]; then
+                echo "specified source is not a directory or directory not found"
+                exit 1
+            fi
+            
+            SOURCE="$(cd "$2" && echo $PWD)"
+            shift
+            ;;
+        -t|--target)
+            if [ ! -d "$2" ]; then
+                echo "specified target is not a directory or directory not found"
+                exit 1
+            fi
+            
+            TARGET="$(cd "$2" && echo $PWD)"
+            shift
+            ;;
+        -n|--name)
+            NAME="$2"
+            shift
+            ;;
+        -h|-\?|--help)
+            showusage
+            exit 1
+            ;;
+        *)
+            echo "unknown argument"
+            exit 1
+            ;;
+    esac
+
+    shift
+done
+
+if [ "$SOURCE" = "" ]; then
+    echo "no source directory specified"
+    exit 1
+elif [ "$SOURCE" = "/" ] && [ "$NAME" = "" ]; then
+    echo "argument -n is mandatory if source points to the root directory"
+    exit 1
+fi
+
+if [ "$TARGET" != "" ]; then
+    if [ ! -d "$TARGET" ]; then
+        echo "specified target is not a directory or directory not found"
+        exit 1
+    fi
+else
+    TARGET=$(dirname "$SOURCE")
+fi
+
+if [ "$TARGET" -ef "$SOURCE" ]; then
+    echo "root and target directory cannot be the same"
+    exit 1
+fi
+
+if [ "$NAME" = "" ]; then
+    NAME="$(basename "$SOURCE")"
+fi
+
+PREV=$(ls -1 $TARGET/$NAME""_*_base.1.dar 2>/dev/null| sort | tail -n 1)
+
+if [ "$PREV" != "" ]; then
+    PREV=$(dirname $PREV)/$(basename $PREV .1.dar)
+    INCR="-A $PREV"
+    PFIX="_diff"
+else
+    PREV=""
+    INCR=""
+    PFIX="_base"
+fi
+
+NAME="$NAME""_$(date -u +"%Y-%m-%dT%H:%M:%SZ")$PFIX"
+FILE="$TARGET/$NAME"
+
+echo "Creating archive: $FILE"
+echo "            from: $SOURCE"
+
+if [ "$PREV" != "" ]; then
+    echo "        previous: $PREV"
+fi
+
+# @see http://dar.sourceforge.net/doc/mini-howto/dar-differential-backup-mini-howto.en.html#making-a-full-backup-with-dar
+# for usage details
+Z=""
+
+for i in "${NO_COMPRESS[@]}"; do
+    Z="$Z -Z \"*.$i\""
+done
+
+dar -m 256 -zbzip2 -y -s 1000G -R "$SOURCE" -c "$FILE" $Z $INCR > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Archive creation FAILED"
+    exit 1
+fi
+
+dar -t "$FILE" > /dev/null
+
+if [ $? -ne 0 ]; then
+    echo "Archive created but test FAILED"
+    exit 1
+else
+    echo "Archive created and successfully tested"
+    exit 1
+fi
---
+++ uncompress.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+if [ "$2" = "" ]; then
+    echo "usage: $(basename $0) <source-base-file> <target-dir>"
+    exit 1
+fi
+
+NUM=$(ls -1 $1_*_base.1.dar 2>/dev/null | wc -l)
+
+if [ $NUM -eq 0 ]; then
+    echo "base file not found for $1"
+    exit 1
+elif [ $NUM -ne 1 ]; then
+    echo "ambigous base file for $1"
+    exit 1
+fi
+
+if [ ! -d "$2" ]; then
+    echo "target directory does not exist"
+    exit 1
+fi
+
+TARGET=$2/$(basename $1 .1.dar)
+
+if [ -e "$TARGET" ]; then
+    echo "target path already exists"
+    exit 1
+fi
+
+for i in $(ls -1 "$1"_*_base.1.dar) $(ls -1 $1_*_diff.1.dar); do
+    echo "Exctracting $i"
+    
+    dar -x "$(dirname $i)/$(basename $i .1.dar)" -O -w -R "$2"
+done
+
+echo "done."
